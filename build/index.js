d16cecf638419531e75b2a50835e18e6b129d969
bobbyhadz <opmftw@gmail.com>
2024-02-29 18:56:15 +0200

    initial commit

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// EXAMPLE 1
+// ---------------------------------------------------
+// EXAMPLE 2
+// ---------------------------------------------------
+// EXAMPLE 3
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSxZQUFZO0FBRVosc0RBQXNEO0FBRXRELFlBQVk7QUFFWixzREFBc0Q7QUFFdEQsWUFBWSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCB7fTtcblxuLy8gRVhBTVBMRSAxXG5cbi8vIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxuXG4vLyBFWEFNUExFIDJcblxuLy8gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tXG5cbi8vIEVYQU1QTEUgM1xuIl19
